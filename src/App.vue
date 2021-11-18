a460665e51d73777fb5539e0a581783ec753fd82
Boitumelo <boitumelotshehla@gmaail.com>
2021-11-18 14:07:08 +0200

    v1.0.5

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,19 @@
 
   <div id="app">
 
-   <router-view :user="user"/>
+   <router-view />
   </div>
 </template>
 
 <script>
 
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 export default {
   
   name: 'App',
 
-  data(){
-    return{
-      user:null,
-    }
-  },
-     async  created() {
-
-           const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            
-            // ..
-            this.user=user;
-            console.log(user.email);
-        } else {
-            // User is signed out
-            // ...
-
-            console.log("You have not signed in. please click login");
-        }
-        });
-            
-        },
+  
 }
 </script>
 
--- src/components/home.vue
+++ src/components/home.vue
@@ -3,77 +3,85 @@
          <div class="auth-wrapper-home">
             <div class="auth-inner-home">
                 <h3>Bus Terminal</h3>
-                <div v-if="isStudData">
-                      <div>
-                          <label for="Name">First Name:</label>
-                          <b-form-input  
-                            type="text" 
-                            id="fname" 
-                            placeholder="Enter your first name"
-                            required
-                            v-model="fName">
-                          </b-form-input>
-                                
-                        </div>
-                         <div>
-                            <label for="last name">Last Name:</label>
-                            <b-form-input  
-                                type="text"
-                                id="lname" 
-                                placeholder="Enter your last name"
-                                required
-                                v-model="lName">
-                            </b-form-input>       
-                          </div>
-                          <div>
-                              <label for="studNum">Student Number:</label>
-                              <b-form-input  
-                                  type="text" 
-                                  id="studNum" 
-                                  placeholder="Enter your student number"
-                                  required
-                                  v-model="studNum">
-                              </b-form-input>
-                            
-                          </div>
-                  </div>
-              <div v-if="!isfilled" class="user-form"> 
-                    <form>
-                        <div class="form-group">
-                                <label for="exampleFormControlSelect1">Location of Departure:</label>
-                                <select class="form-control" v-model="select.depature" @change="depature(select.depature)">
-                                 <option value="" disabled selected>Select your option</option>
-                                <option v-for="Campus in Campuses" :key="Campus">{{Campus}}</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">Destination:</label>
-                                <select class="form-control" v-model="select.destination" @change="destination(select.destination)">
-                                 <option value="" disabled selected>Select your option</option>
-                                <option v-for="Campus in Campuses" :key="Campus">{{Campus}}</option>
-                                </select>
-                            </div>
-                             <div class="form-group">
-                                <label for="exampleFormControlSelect1">Time of depature:</label>
-                                <select class="form-control" v-model="select.time" @change="timeDep(select.time)">
-                                 <option value="" disabled selected>Select your option</option>
-                                <option v-for="time in times" :key="time">{{time}}</option>
-                                </select>
-                            </div>
-                    </form>
-                </div>
-                <div v-if="isfilled">
-                    <qr-code class="qr-space" :text="qrText"></qr-code> 
-                </div>
-                <div>
-                  <div v-if="!isfilled">
-                    <b-button variant="success" @click="handleSubmit">Book transport</b-button>
-                  </div>
-                  <div v-if="isfilled">
-                    <b-button class="btn" variant="danger" @click="cancel">Cancel transport</b-button>
-                  </div>
+          <div v-if="!isLoaded">
+                
+                <div class="d-flex justify-content-center mb-3">
+                  <b-spinner label=""></b-spinner>
                 </div>
           </div>
+          <div v-if="isLoaded">
+                  <div v-if="!isStudData">
+                        <div>
+                            <label for="Name">First Name:</label>
+                            <b-form-input  
+                              type="text" 
+                              id="fname" 
+                              placeholder="Enter your first name"
+                              required
+                              v-model="fName">
+                            </b-form-input>
+                                  
+                          </div>
+                          <div>
+                              <label for="last name">Last Name:</label>
+                              <b-form-input  
+                                  type="text"
+                                  id="lname" 
+                                  placeholder="Enter your last name"
+                                  required
+                                  v-model="lName">
+                              </b-form-input>       
+                            </div>
+                            <div>
+                                <label for="studNum">Student Number:</label>
+                                <b-form-input  
+                                    type="text" 
+                                    id="studNum" 
+                                    placeholder="Enter your student number"
+                                    required
+                                    v-model="studNum">
+                                </b-form-input>
+                              
+                            </div>
+                    </div>
+                <div v-if="!isfilled" class="user-form"> 
+                      <form>
+                          <div class="form-group">
+                                  <label for="exampleFormControlSelect1">Location of Departure:</label>
+                                  <select class="form-control" v-model="select.depature" @change="depature(select.depature)">
+                                  <option value="" disabled selected>Select your option</option>
+                                  <option v-for="Campus in Campuses" :key="Campus">{{Campus}}</option>
+                                  </select>
+                              </div>
+                              <div class="form-group">
+                                  <label for="exampleFormControlSelect1">Destination:</label>
+                                  <select class="form-control" v-model="select.destination" @change="destination(select.destination)">
+                                  <option value="" disabled selected>Select your option</option>
+                                  <option v-for="Campus in Campuses" :key="Campus">{{Campus}}</option>
+                                  </select>
+                              </div>
+                              <div class="form-group">
+                                  <label for="exampleFormControlSelect1">Time of depature:</label>
+                                  <select class="form-control" v-model="select.time" @change="timeDep(select.time)">
+                                  <option value="" disabled selected>Select your option</option>
+                                  <option v-for="time in times" :key="time">{{time}}</option>
+                                  </select>
+                              </div>
+                      </form>
+                  </div>
+                  <div v-if="isfilled">
+                      <qr-code class="qr-space" :text="qrText"></qr-code> 
+                  </div>
+                  <div>
+                    <div v-if="!isfilled">
+                      <b-button variant="success" @click="handleSubmit">Book transport</b-button>
+                    </div>
+                    <div v-if="isfilled">
+                      <b-button class="btn" variant="danger" @click="cancel">Cancel transport</b-button>
+                    </div>
+                  </div>
+              </div>
+            </div>
          </div>
     </div>
 </template>
@@ -81,15 +89,16 @@
 <script>
 
 import { doc, getDoc, getFirestore,setDoc } from "firebase/firestore";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 
 export default {
     name:'home',
-    props:['user'],
      data() {
       return {
        isfilled:false,
+       isStudData:false,
+       isLoaded:false,
        types:null,
        select:{
          depature:null,
@@ -102,19 +111,36 @@ export default {
        studCount:null,
        today:null,
        qrText:null,
-       isStudData:true,
+       
        fName:null,
        lName:null,
-       studNum:null
+       studNum:null,
+       user:null,
       }
     },
     async created(){
-      console.log(this.user.email);
+       const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            
+            this.user=user;
+             this. getUserData(user);
+        } else {
+            // User is signed out
+            // ...
+
+            console.log("You have not signed in. please click login");
+        }
+        });
+
+      
       
     },
     methods: {
      async  handleSubmit(){
-        
+      this.isLoaded=false;
       const db=getFirestore();
       var today = new Date();
       var dd = today.getDate();
@@ -122,18 +148,24 @@ export default {
       var yyyy = today.getFullYear();
 
       this.today=dd+'/'+mm+'/'+yyyy;
-
-        if(this.isStudData){
-          this.isStudData=false;
-        // Add a new document in collection "Students"
-        await setDoc(doc(db,"students",this.user.uid),{
-          fname:this.fName,
-          lname:this.lName,
-          studNum:this.studNum,
-          isStudData:this.isStudData
-        });
-        
-      }
+      this.isfilled=true;
+     
+       
+          this.isStudData=true;
+          // Add a new document in collection "Students"
+          await setDoc(doc(db,"students",this.user.uid),{
+            fname:this.fName,
+            lname:this.lName,
+            studNum:this.studNum,
+            isStudData:this.isStudData,
+            isfilled:this.isfilled,
+            depature:this.select.depature,
+            destination:this.select.destination,
+            time:this.select.time,
+            date:this.today,
+          });
+          
+      
 
       //Transport Bookin
       const docRef = doc(db, "Campus", this.select.depature,this.select.destination,this.select.time);
@@ -141,26 +173,48 @@ export default {
        if (docSnap.exists()) {
           console.log("Document data:", docSnap.data().studCount);
           this.studCount=docSnap.data().studCount+1;
-          this.isfilled=true;
           this.qrText=this.select.depature+" to "+this.select.destination+" ("+this.select.time+" "+this.today+")";
 
            // Add a new document in collection "Campus"
           await setDoc(doc(db, "Campus", this.select.depature,this.select.destination,this.select.time), {
             studCount:this.studCount,
           });
+          this.isLoaded=true;
         } else {
           // doc.data() will be undefined in this case
           console.log("No such document!");
         }
+        
        },
+
+
+
+       //Cancel Bus Bookin
       async cancel(){
+        this.isLoaded=false;
+        this.isfilled=false; 
          const db=getFirestore();
-         // Add a new document in collection "cities"
+         // Add a new document in collection "Campus"
           await setDoc(doc(db, "Campus", this.select.depature,this.select.destination,this.select.time), {
             studCount:this.studCount-1,
           });
-          this.isfilled=false;
+    
+          await setDoc(doc(db,"students",this.user.uid),{
+            fname:this.fName,
+            lname:this.lName,
+            studNum:this.studNum,
+            isStudData:this.isStudData,
+            isfilled:this.isfilled,
+            depature:"",
+            destination:"",
+            time:"",
+            date:"",
+          
+          });
+          this.isLoaded=true;
        },
+
+
         depature(value){
         var name = value;
         console.log(name);
@@ -171,7 +225,35 @@ export default {
         },
         timeDep(value){
           console.log(value);
-        }
+        },
+
+
+
+       async getUserData(user){
+          const db=getFirestore();
+          const userId=user.uid;
+          console.log(userId);
+          const docRef = doc(db, "students", userId);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            this.isStudData=docSnap.data().isStudData;
+            this.isfilled=docSnap.data().isfilled;
+            this.fName=docSnap.data().fname;
+            this.lName=docSnap.data().lname;
+            this.studNum=docSnap.data().studNum;
+            this.select.depature=docSnap.data().depature;
+            this.select.destination=docSnap.data().destination;
+            this.select.time=docSnap.data().time;
+            this.today=docSnap.data().date;
+            this.qrText=this.select.depature+" to "+this.select.destination+" ("+this.select.time+" "+this.today+")";
+            console.log(docSnap.data());
+            this.isLoaded=true;
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        },
     },  
 }
 </script>
